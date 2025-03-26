602ded9780b44db98bcccf728655b1484d6f7da9
bunny70pl <bunny70pl@gmail.com>
2025-03-26 13:20:22 +0100

    Dao

---
+++ app/src/main/java/com/example/bazadanych3a/DaoPracownicy.java
@@ -0,0 +1,33 @@
+package com.example.bazadanych3a;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.ArrayList;
+
+import kotlinx.coroutines.DelicateCoroutinesApi;
+
+@Dao
+public interface DaoPracownicy {
+    @Insert
+    public void dodajPracownika(Pracownik pracownik);
+
+    @Insert
+    public void dodajWieluPracownikow(Pracownik ...pracownicy);
+
+    @Delete
+    public void usunPracownika(Pracownik pracownik);
+
+    @Update
+    public void zaktualizujDanePracownika();
+
+    @Query("Select * from Pracownicy where jezykOjczysty='Polski'")
+    public ArrayList<Pracownik> wypiszPracownikowGdzieJezykToPolski();
+
+    @Query("Select * from pracownicy where jezykObcyKomunikatywny = :jezyk")
+    public ArrayList<Pracownik> wypiszPracownikowMowiacychWJezuku(String jezyk);
+
+}
